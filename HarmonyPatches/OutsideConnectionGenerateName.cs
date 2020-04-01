84b13dd0457c6bd489f2310386ac1ac6e8abc385
DNKpp <DNKpp2011@gmail.com>
2020-04-02 01:39:15 +0200

    improve GenerateName patch
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- HarmonyPatches/OutsideConnectionGenerateName.cs
+++ HarmonyPatches/OutsideConnectionGenerateName.cs
@@ -1,27 +1,53 @@
-﻿using Harmony;
-using ICities;
-using ColossalFramework;
-using System.Collections.Generic;
+﻿using ColossalFramework.Math;
+using Harmony;
+using UnityEngine;
 
 namespace ImprovedOutsideConnection.HarmonyPatches
 {
     [HarmonyPatch(typeof(OutsideConnectionAI))]
     [HarmonyPatch("GenerateName")]
-    //[HarmonyPatch(new Type[] { typeof(int) })]
     class OutsideConnectionGenerateName
     {
-        private static Dictionary<ushort, string> m_OutsideConnectionNameDict = new Dictionary<ushort, string>();
-
         private static bool Prefix(ushort buildingID, ref string __result)
         {
-            //var outsideConnections = BuildingManager.instance.GetOutsideConnections();
-
-            if (!m_OutsideConnectionNameDict.TryGetValue(buildingID, out __result))
+            var settings = OutsideConnectionSettingsManager.instance.m_SettingsDict;
+            OutsideConnectionSettings setting = null;
+            if (settings.TryGetValue(buildingID, out setting))
             {
-                __result = "Test" + m_OutsideConnectionNameDict.Count;
-                m_OutsideConnectionNameDict.Add(buildingID, __result);
+                switch (setting.NameMode)
+                {
+                    case OutsideConnectionSettings.NameModeType.CustomSingle:
+                        {
+                            var textArr = setting.NameText.Split(';');
+                            if (textArr.Length != 0)
+                            {
+                                __result = textArr[0];
+                                return false;
+                            }
+                            break;
+                        }
+
+                    case OutsideConnectionSettings.NameModeType.CustomRandom:
+                        {
+                            var textArr = setting.NameText.Split(';');
+                            if (textArr.Length != 0)
+                            {
+                                Randomizer r = new Randomizer(buildingID);
+                                __result = textArr[r.Int32(0, textArr.Length - 1)];
+                                return false;
+                            }
+                            break;
+                        }
+
+                    case OutsideConnectionSettings.NameModeType.Vanilla:    // just follow as usual
+                    default:
+                        break;
+                }
             }
-            return false;    // Well, we skip the original code, because it isn't necessary and this way we safe some performance. Are there any good reasons not to do?
+            else
+                Debug.Log("ImprovedOutsideConnection: GenerateName: No settings for OutsideConnection found.");
+
+            return true;    // Well, we skip the original code, because it isn't necessary and this way we safe some performance. Are there any good reasons not to do?
         }
     }
 }
