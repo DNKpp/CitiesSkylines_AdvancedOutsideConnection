ef59425e0b566c6ad714f2281da9cecf3e7919b6
DNKpp <DNKpp2011@gmail.com>
2020-04-03 15:48:57 +0200

    improve basic mod behavior
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- ImprovedOutsideConnection.cs
+++ ImprovedOutsideConnection.cs
@@ -1,6 +1,7 @@
 ﻿using ICities;
 using Harmony;
 using UnityEngine;
+using ColossalFramework.UI;
 
 namespace ImprovedOutsideConnection
 {
@@ -14,15 +15,31 @@ namespace ImprovedOutsideConnection
 
         public static bool InGame { get; internal set; } = false;
 
+        private static string m_HarmonyIdentifier = "connection.outside.improved";
+
         HarmonyInstance m_HarmonyInstance = null;
 
         public static SettingsGUI m_SettingsGUI = null;
+
+        private GameObject m_IOCGameObject = null;
 
         public override void OnCreated(ILoading loading)
         {
             base.OnCreated(loading);
 
-            m_HarmonyInstance = HarmonyInstance.Create("connection.outside.improved");
+            m_HarmonyInstance = HarmonyInstance.Create(m_HarmonyIdentifier);
+
+
+
+            if (loading.loadingComplete)
+            {
+                switch (loading.currentMode)
+                {
+                    case AppMode.Game:
+                        Init();
+                        break;
+                }
+            }
         }
 
         public override void OnLevelLoaded(LoadMode mode)
@@ -33,14 +50,7 @@ namespace ImprovedOutsideConnection
             {
                 case LoadMode.NewGame:
                 case LoadMode.LoadGame:
-                    InGame = true;
-
-                    m_HarmonyInstance.PatchAll();
-
-                    OutsideConnectionSettingsManager.instance.SyncWithBuildingManager();
-
-                    var cameraController = GameObject.FindObjectOfType<CameraController>();
-                    m_SettingsGUI = cameraController.gameObject.AddComponent<SettingsGUI>();
+                    Init();
                     break;
             }
         }
@@ -50,7 +60,8 @@ namespace ImprovedOutsideConnection
             base.OnReleased();
             if (InGame)
             {
-                m_HarmonyInstance.UnpatchAll();
+                Deinit();
+                m_HarmonyInstance = null;
             }
         }
 
@@ -61,13 +72,35 @@ namespace ImprovedOutsideConnection
             if (!InGame)
                 return;
 
+            Deinit();
+        }
+
+        private void Init()
+        {
+            InGame = true;
+            m_HarmonyInstance.PatchAll();
+            OutsideConnectionSettingsManager.instance.SyncWithBuildingManager();
+
+            //var cameraController = GameObject.FindObjectOfType<CameraController>();
+            //m_SettingsGUI = cameraController.gameObject.AddComponent<SettingsGUI>();
+
+            var objectOfType = UnityEngine.Object.FindObjectOfType<UIView>();
+            m_IOCGameObject = new GameObject("IOCGameObject");
+            m_IOCGameObject.transform.parent = objectOfType.transform;
+            m_IOCGameObject.AddComponent<PanelExtenderOutsideConnections>();
+        }
+
+        private void Deinit()
+        {
             try
             {
-                m_HarmonyInstance.UnpatchAll();
-                m_HarmonyInstance = null;
+                m_HarmonyInstance.UnpatchAll(m_HarmonyIdentifier);
 
                 GameObject.Destroy(m_SettingsGUI);
                 m_SettingsGUI = null;
+
+                if (m_IOCGameObject)
+                    UnityEngine.Object.Destroy(m_IOCGameObject);
             }
             finally
             {
