54fe3855d197150b697273919d0d5efaa57cf8aa
DNKpp <DNKpp2011@gmail.com>
2020-04-07 01:15:22 +0200

    make base class TableRowComponent
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- OutsideConnectionInfo.cs
+++ OutsideConnectionInfo.cs
@@ -3,64 +3,40 @@ using UnityEngine;
 
 namespace AdvancedOutsideConnection
 {
-    class OutsideConnectionInfo : UICustomControl
+    class OutsideConnectionInfo : TableRowComponent
     {
         private ushort m_BuildingID = 0;
         private UIPanel m_MainPanel = null;
-        private UIPanel m_Background = null;
         private UISprite m_TransportTypeSprite = null;
         private UITextField m_ConnectionNameTextfield = null;
-        private Color32 m_BackgroundColor = new Color32(49, 52, 58, 0);
         private UIButton m_DetailViewButton = null;
-        private bool m_IsMouseOver = false;
         private OutsideConnectionSettings m_CachedSettings = null;
         public OutsideConnectionSettings currentSettings => m_CachedSettings;
 
-        private void Awake()
+        private new void Awake()
         {
             m_MainPanel = gameObject.AddComponent<UIPanel>();
             m_MainPanel.name = "MainPanel";
             m_MainPanel.size = new Vector2(400, 45);
 
-            m_Background = m_MainPanel.AddUIComponent<UIPanel>();
-            m_Background.name = "BackgroundPanel";
-            m_Background.backgroundSprite = "InfoviewPanel";
-            m_Background.position = Vector3.zero;
-            m_Background.size = m_MainPanel.size;
-            m_Background.anchor = UIAnchorStyle.All;
+            base.Awake();
 
             m_TransportTypeSprite = m_MainPanel.AddUIComponent<UISprite>();
             m_TransportTypeSprite.anchor = UIAnchorStyle.Left | UIAnchorStyle.CenterVertical;
             m_TransportTypeSprite.size = new Vector2(28, 28);
             m_TransportTypeSprite.relativePosition = new Vector3(12, 8);
 
-            m_ConnectionNameTextfield = m_MainPanel.AddUIComponent<UITextField>();
-            m_ConnectionNameTextfield.size = new Vector2(100, 35);
+            m_ConnectionNameTextfield = WidgetsFactory.AddTextField(m_MainPanel);
+            m_ConnectionNameTextfield.size = new Vector2(175, 35);
             m_ConnectionNameTextfield.relativePosition = new Vector3(100, 10);
-            m_ConnectionNameTextfield.horizontalAlignment = UIHorizontalAlignment.Center;
-            m_ConnectionNameTextfield.verticalAlignment = UIVerticalAlignment.Middle;
             m_ConnectionNameTextfield.anchor = UIAnchorStyle.Right | UIAnchorStyle.Left | UIAnchorStyle.CenterVertical;
-            m_ConnectionNameTextfield.textScale = 1f;
-            m_ConnectionNameTextfield.font = WidgetsFactory.instance.textFont;
-            m_ConnectionNameTextfield.textColor = new Color32(185, 221, 254, 255);
-            m_ConnectionNameTextfield.multiline = false;
-            m_ConnectionNameTextfield.readOnly = false;
-            m_ConnectionNameTextfield.cursorBlinkTime = 0.45f;
-            m_ConnectionNameTextfield.cursorWidth = 1;
-            m_ConnectionNameTextfield.selectionSprite = "EmptySprite";
-            m_ConnectionNameTextfield.focusedBgSprite = "TextFieldPanel";
-            m_ConnectionNameTextfield.hoveredBgSprite = "TextFieldPanelHovered";
-            m_ConnectionNameTextfield.selectionBackgroundColor = Color.white;
-            m_ConnectionNameTextfield.allowFloats = false;
-            m_ConnectionNameTextfield.numericalOnly = false;
-            m_ConnectionNameTextfield.allowNegative = false;
-            m_ConnectionNameTextfield.isPasswordField = false;
-            m_ConnectionNameTextfield.builtinKeyNavigation = true;
-            m_ConnectionNameTextfield.padding = new RectOffset(8, 3, 5, 5);
             m_ConnectionNameTextfield.eventTextSubmitted += delegate (UIComponent component, string newText)
             {
-                if (m_CachedSettings != null)
+                if (m_BuildingID != 0 && m_CachedSettings != null)
+                {
                     m_CachedSettings.Name = newText;
+                    eventNameChanged?.Invoke(m_BuildingID, newText);
+                }
             };
 
             m_DetailViewButton = m_MainPanel.AddUIComponent<UIButton>();
@@ -74,22 +50,11 @@ namespace AdvancedOutsideConnection
             m_DetailViewButton.pressedBgSprite = "LineDetailButtonPressed";
             //m_DetailViewButton.disabledBgSprite = "";
 
-            component.eventZOrderChanged += delegate
+            m_DetailViewButton.eventClick += delegate
             {
-                SetBackgroundColor();
+                if (m_BuildingID != 0 && eventDetailsOpen != null)
+                    eventDetailsOpen(m_BuildingID);
             };
-
-            //m_DetailViewButton.eventClick += delegate
-            //{
-            //    if (m_BuildingID != 0)
-            //    {
-            //        //Vector3 position = Singleton<NetManager>.instance.m_nodes.m_buffer[Singleton<TransportManager>.instance.m_lines.m_buffer[m_LineID].m_stops].m_position;
-            //        //InstanceID instanceID = default(InstanceID);
-            //        //instanceID.Building = m_LineID;
-            //        //WorldInfoPanel.Show<PublicTransportWorldInfoPanel>(position, instanceID);
-            //        //ToolsModifierControl.cameraController.SetTarget(instanceID, position, zoomIn: true);
-            //    }
-            //};
 
             component.eventVisibilityChanged += delegate (UIComponent comp, bool isVisible)
             {
@@ -100,6 +65,9 @@ namespace AdvancedOutsideConnection
             };
         }
 
+        public event DetailsOpenEventHandler eventDetailsOpen;
+        public event OutsideConnectionPropertyChanged<string> eventNameChanged;
+
         public ushort buildingID
         {
             get { return m_BuildingID; }
@@ -108,30 +76,12 @@ namespace AdvancedOutsideConnection
 
         public void RefreshData()
         {
-
             if (m_BuildingID == 0 || !OutsideConnectionSettingsManager.instance.SettingsDict.TryGetValue(m_BuildingID, out m_CachedSettings))
                 return;
 
-            SetupTransportTypeIcon();
+            var transportInfo = Utils.QueryTransportInfo(m_BuildingID);
+            m_TransportTypeSprite.spriteName = CommonSpriteNames.SubBarPublicTransport[(int)transportInfo.m_transportType];
             m_ConnectionNameTextfield.text = m_CachedSettings.Name;
-        }
-
-        private void SetupTransportTypeIcon()
-        {
-            m_TransportTypeSprite.spriteName = CommonSpriteNames.SubBarPublicTransport[(int)m_CachedSettings.Type];
-        }
-
-        public void SetBackgroundColor()
-        {
-            Color32 backgroundColor = m_BackgroundColor;
-            backgroundColor.a = (byte)((component.zOrder % 2 != 0) ? 127 : 255);
-            if (m_IsMouseOver)
-            {
-                backgroundColor.r = (byte)Mathf.Min(255, backgroundColor.r * 3 >> 1);
-                backgroundColor.g = (byte)Mathf.Min(255, backgroundColor.g * 3 >> 1);
-                backgroundColor.b = (byte)Mathf.Min(255, backgroundColor.b * 3 >> 1);
-            }
-            m_Background.color = backgroundColor;
         }
 
         private void SetBuildingID(ushort id)
@@ -139,24 +89,6 @@ namespace AdvancedOutsideConnection
             m_BuildingID = id;
 
             RefreshData();
-        }
-
-        private void OnMouseEnter(UIComponent comp, UIMouseEventParameter param)
-        {
-            if (!m_IsMouseOver)
-            {
-                m_IsMouseOver = true;
-                SetBackgroundColor();
-            }
-        }
-
-        private void OnMouseLeave(UIComponent comp, UIMouseEventParameter param)
-        {
-            if (m_IsMouseOver)
-            {
-                m_IsMouseOver = false;
-                SetBackgroundColor();
-            }
         }
     }
 }
---
+++ TableRowComponent.cs
@@ -0,0 +1,67 @@
+﻿using ColossalFramework.UI;
+using UnityEngine;
+
+namespace AdvancedOutsideConnection
+{
+    class TableRowComponent : UICustomControl
+    {
+        private bool m_IsMouseOver = false;
+        public bool isMouseOver => m_IsMouseOver;
+
+        private UIPanel m_Background = null;
+        private Color32 m_BackgroundColor = new Color32(49, 52, 58, 0);
+
+        protected void Awake()
+        {
+            if (!component)
+                throw new System.Exception("TableRowComponent: You must setup component first before calling base.Awake()");
+
+            m_Background = component.AddUIComponent<UIPanel>();
+            m_Background.name = "BackgroundPanel";
+            m_Background.backgroundSprite = "InfoviewPanel";
+            m_Background.position = Vector3.zero;
+            m_Background.size = component.size;
+            m_Background.anchor = UIAnchorStyle.All;
+            SetBackgroundColor();
+
+            component.eventMouseEnter += OnMouseEnter;
+            component.eventMouseLeave += OnMouseLeave;
+
+            component.eventZOrderChanged += delegate
+            {
+                SetBackgroundColor();
+            };
+        }
+
+        public void SetBackgroundColor()
+        {
+            Color32 backgroundColor = m_BackgroundColor;
+            backgroundColor.a = (byte)((component.zOrder % 2 != 0) ? 127 : 255);
+            if (m_IsMouseOver)
+            {
+                backgroundColor.r = (byte)Mathf.Min(255, backgroundColor.r * 3 >> 1);
+                backgroundColor.g = (byte)Mathf.Min(255, backgroundColor.g * 3 >> 1);
+                backgroundColor.b = (byte)Mathf.Min(255, backgroundColor.b * 3 >> 1);
+            }
+            m_Background.color = backgroundColor;
+        }
+
+        private void OnMouseEnter(UIComponent comp, UIMouseEventParameter param)
+        {
+            if (!m_IsMouseOver)
+            {
+                m_IsMouseOver = true;
+                SetBackgroundColor();
+            }
+        }
+
+        private void OnMouseLeave(UIComponent comp, UIMouseEventParameter param)
+        {
+            if (m_IsMouseOver)
+            {
+                m_IsMouseOver = false;
+                SetBackgroundColor();
+            }
+        }
+    }
+}
