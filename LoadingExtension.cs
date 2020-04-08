90be3912c8b2528d8f2d5e9441dbdd50b892b872
DNKpp <DNKpp2011@gmail.com>
2020-04-08 10:46:09 +0200

    add boost license
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- LoadingExtension.cs
+++ LoadingExtension.cs
@@ -1,7 +1,12 @@
-﻿using ICities;
+﻿
+//          Copyright Dominic Koepke 2020 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+using ICities;
 using Harmony;
 using UnityEngine;
-using ColossalFramework.UI;
 using ColossalFramework.Plugins;
 using System.Collections.Generic;
 
--- OutsideConnectionDetailPanel.cs
+++ OutsideConnectionDetailPanel.cs
@@ -1,4 +1,10 @@
-﻿using ColossalFramework.UI;
+﻿
+//          Copyright Dominic Koepke 2020 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+using ColossalFramework.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
