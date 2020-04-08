ab72b4eeac2f022ac78f689df9a4ece45c51b530
DNKpp <DNKpp2011@gmail.com>
2020-04-08 12:03:31 +0200

    use Postfix instead of Prefix for GenerateName
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- HarmonyPatches/OutsideConnectionGenerateName.cs
+++ HarmonyPatches/OutsideConnectionGenerateName.cs
@@ -13,7 +13,7 @@ namespace AdvancedOutsideConnection.HarmonyPatches
     [HarmonyPatch("GenerateName")]
     class OutsideConnectionGenerateName
     {
-        private static bool Prefix(ushort buildingID, InstanceID caller, ref string __result)
+        private static void Postfix(ushort buildingID, InstanceID caller, ref string __result)
         {
             var settings = OutsideConnectionSettingsManager.instance.SettingsDict;
             OutsideConnectionSettings setting = null;
@@ -24,10 +24,7 @@ namespace AdvancedOutsideConnection.HarmonyPatches
                     case OutsideConnectionSettings.NameModeType.CustomSingle:
                         {
                             if (setting.SingleGenerationName != string.Empty)
-                            {
                                 __result = setting.SingleGenerationName;
-                                return false;   // Well, we skip the original code, because it isn't necessary and this way we safe some performance. Are there any good reasons not to do?
-                            }
                             break;
                         }
 
@@ -37,7 +34,6 @@ namespace AdvancedOutsideConnection.HarmonyPatches
                             {
                                 Randomizer r = new Randomizer(caller.Index);
                                 __result = setting.RandomGenerationNames[r.Int32((uint)setting.RandomGenerationNames.Count)];
-                                return false;   // Well, we skip the original code, because it isn't necessary and this way we safe some performance. Are there any good reasons not to do?
                             }
                             break;
                         }
@@ -49,8 +45,6 @@ namespace AdvancedOutsideConnection.HarmonyPatches
             }
             else
                 Utils.Log("AdvancedOutsideConnection: GenerateName: No settings for OutsideConnection found.");
-
-            return true;
         }
     }
 }
