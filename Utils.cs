7e7fa0072674e42ee9dda7ef3030f0c75fb5bc6f
DNKpp <dnkpp2011@gmail.com>
2021-06-28 09:54:59 +0200

    cleanup usings
    Signed-off-by: DNKpp <dnkpp2011@gmail.com>

--- Utils.cs
+++ Utils.cs
@@ -4,7 +4,6 @@
 //	(See accompanying file LICENSE_1_0.txt or copy at
 //		  https://www.boost.org/LICENSE_1_0.txt)
 
-using ColossalFramework.PlatformServices;
 using ColossalFramework.UI;
 using System;
 using System.Linq;
